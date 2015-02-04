33e034c8d7d3f08a2c2dd85a65711e07f1423d77
LeoHoogerbrugge <hoogerbrugge.leo@gmail.com>
2015-02-04 12:20:31 +0100

    Floats in potentials

--- Potentials.py
+++ Potentials.py
@@ -5,7 +5,7 @@ import numpy as np
 def Len_Jones(Pos1, Pos2):
     diff = np.subtract(Pos1,Pos2)
     diff_squaresum = np.sum(np.square(diff))
-    Potential = 4.0 * ( (1 / diff_squaresum**6) - (1 / diff_squaresum**3) )
+    Potential = 4.0 * ( (1.0 / diff_squaresum**6) - (1.0 / diff_squaresum**3) )
     Force_factor = 4.0 * ( (12.0 / diff_squaresum**7) - (6.0 / diff_squaresum**4) )
     Force1 = np.multiply(diff,[Force_factor,Force_factor,Force_factor])
     Force2 = np.negative(Force1)
@@ -14,5 +14,5 @@ def Len_Jones(Pos1, Pos2):
 # Gravitational potential along z-axis
 def Gravity(Pos,Grav_const):
     Potential = Grav_const * Pos[2]
-    Force = [0,0,-Grav_const]
+    Force = [0.0,0.0,-Grav_const]
     return Force, Potential
