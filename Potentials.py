5a22403ad05b241296f06f4c19ad8ebc6ef0d87e
LeoHoogerbrugge <hoogerbrugge.leo@gmail.com>
2015-02-04 15:32:05 +0100

    Vectorized potentials, N particles

--- Potentials.py
+++ Potentials.py
@@ -1,17 +1,44 @@
 # Calculating potentials and forces
+import numpy as np
 
-# Lennard - Jones, sigma and epsilon not yet chosen!
-def Len_Jones(Pos1, Pos2):
-    diff = Pos1 - Pos2
-    diff_squaresum = sum(diff**2)
-    Potential = 4.0 * ( (1.0 / diff_squaresum**6) - (1.0 / diff_squaresum**3) )
+# Leonard - Jones, sigma and epsilon not yet chosen!
+# WARNING: Code is vectorised, i.e. it trades memory for speed. If memory becomes an issue, remove matrixes after they
+# have been used, as the code creates multiple size 3*N^2 matrices for calculations
+def Len_Jones(Pos_mat):
+    shape = np.shape(Pos_mat)
+    # Copy the position matrix N times (N = shape[0] = number of particles), result = N x 3*N
+    Pos_rep = np.kron(np.ones((1,shape[0])),Pos_mat)
+    # Create a 'transpose' of the repeated matrix, result = N x 3*N
+    Pos_vec = np.reshape(Pos_mat,(1,shape[0]*shape[1]))
+    Pos_repT = np.kron(np.ones((shape[0],1)),Pos_vec)
+    # Subtract the two to get the relative distances
+    diff_mat = Pos_rep - Pos_repT
+    # Get the sum of the square distances (includes some awkward reshapes to facilitate summing)
+    diff_square = diff_mat**2
+    diff_square = np.reshape(diff_square,(shape[0]**2,shape[1]))
+    diff_squaresum = np.sum(diff_square,axis=1)
+    diff_squaresum = np.reshape(diff_squaresum,(shape[0],shape[0]))
+    # Replace zeros by infinity to avoid division by zero
+    diff_squaresum = (diff_squaresum == 0).choose(diff_squaresum,float("inf"))
+    # Calculate potential and forces using Leonard-Jones
+    Potential_vec = 4.0 * ( (1.0 / diff_squaresum**6) - (1.0 / diff_squaresum**3) )
+    Potential = 0.5 * np.sum(Potential_vec,axis=None)
     Force_factor = 4.0 * ( (12.0 / diff_squaresum**7) - (6.0 / diff_squaresum**4) )
-    Force1 = Force_factor * diff
-    Force2 = -Force1
-    return Force1, Force2, Potential
+    # Create a N x 3*N matrix for the force factor
+    Force_factor = Force_factor.repeat(shape[1])
+    Force_factor = np.reshape(Force_factor,(shape[0],shape[0]*shape[1]))
+    # Calculate the force vectors by multiplying the force factor and the difference vectors
+    Force_mat = Force_factor * diff_mat
+    # Get the force on each particle by summing contributions of all particles
+    Force = np.sum(Force_mat,axis=0)
+    # Reshape to a N x 3 matrix
+    Force = np.reshape(Force,shape)
+    return Force, Potential
 
-# Gravitational potential along z-axis
-def Gravity(Pos,Grav_const):
-    Potential = Grav_const * Pos[2]
-    Force = [0.0,0.0,-Grav_const]
+
+#Gravitational potential along z-axis
+def Gravity(Pos_mat,Grav_const):
+    shape = np.shape(Pos_mat)
+    Potential = sum(Grav_const * Pos_mat[:,2])
+    Force = np.kron(np.ones((shape[0],1)),[0.0,0.0,-Grav_const])
     return Force, Potential
