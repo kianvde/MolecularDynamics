ee8fd2ec0a4247755e0d90e28fb9da782f1fd1f5
LeoHoogerbrugge <hoogerbrugge.leo@gmail.com>
2015-02-04 12:35:40 +0100

    Update Potentials to use numpy arrays

--- Potentials.py
+++ Potentials.py
@@ -1,14 +1,13 @@
 # Calculating potentials and forces
-import numpy as np
 
 # Lennard - Jones, sigma and epsilon not yet chosen!
 def Len_Jones(Pos1, Pos2):
-    diff = np.subtract(Pos1,Pos2)
-    diff_squaresum = np.sum(np.square(diff))
+    diff = Pos1 - Pos2
+    diff_squaresum = sum(diff**2)
     Potential = 4.0 * ( (1.0 / diff_squaresum**6) - (1.0 / diff_squaresum**3) )
     Force_factor = 4.0 * ( (12.0 / diff_squaresum**7) - (6.0 / diff_squaresum**4) )
-    Force1 = np.multiply(diff,[Force_factor,Force_factor,Force_factor])
-    Force2 = np.negative(Force1)
+    Force1 = Force_factor * diff
+    Force2 = -Force1
     return Force1, Force2, Potential
 
 # Gravitational potential along z-axis
