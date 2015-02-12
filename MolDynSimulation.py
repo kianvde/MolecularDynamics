dd932e6de1da9d115ebaa8a4057e65dc1f4756b7
kianvde <neal@wearemed.nl>
2015-02-12 14:00:33 +0100

    inserted animation objects needed for vpy_animate module

--- MolDynSimulation.py
+++ MolDynSimulation.py
@@ -1,6 +1,6 @@
 import variables as var
 import numpy as np
-import animate as an
+import vpy_animate as an
 
 class MolDynSimulation(object) :
 
@@ -15,18 +15,19 @@ class MolDynSimulation(object) :
         np.set_printoptions(precision = 2)
         print(self.particles.positions)
         # Number of simulation loops
-        loops = 2
+        loops = 10
         #Create animation object
-        self.animation = an.Animate(var.boxSize, loops, var.dimension, var.numParticles)
+        #self.animation = an.Animate(var.boxSize, loops, var.dimension, var.numParticles)
+        self.animation = an.VpyAnimate(self.particles, loops)
 
         for i in range(loops):
             #Build coordinate matrix for every iteration of the loop
-            self.animation.buildLines(i, self.particles.positions)
+            #self.animation.buildCoords(i, self.particles.positions)
             # update particles
             self.particles.update(var.deltaT)
         #Resize axis and do animation
         self.animation.plot_anim()
-
+        #self.particles.
 
 # init and loop
 molDynSimulation = MolDynSimulation()
